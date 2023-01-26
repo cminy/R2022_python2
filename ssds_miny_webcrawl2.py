f219aee1ce4e18269208b043e845bf1bd7405fb6
miny <mksuuup@gmail.com>
2023-01-26 15:08:56 +0900

    플레이리스트 뽑아내기 #1

--- ssds_miny_webcrawl2.py
+++ ssds_miny_webcrawl2.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
-# import string
-# en_list = [e for e in string.ascii_lowercase]
-# letters = kor_list + en_list
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import time
 
 # set driver
 def set_chrome_driver():
@@ -15,17 +15,25 @@ def set_chrome_driver():
     return driver
 
 new_driver = set_chrome_driver()
-yt_url = "https://youtu.be/mnpQsM-tqQU"
+yt_url = "https://youtube.com/playlist?list=FLN3QmzfUO5BvjP5qVpRQfjg"
 new_driver.get(yt_url)
 
-yt = new_driver.find_element(By.CSS_SELECTOR, value='a#video-title')
-print(yt.get_attribute('title'), yt.get_attribute('href'), sep='----')
+yts = new_driver.find_elements(By.CSS_SELECTOR, value='a#video-title')
+titleList = []
+hrefList = []
 
+print ('>>>> start')
+for yt in yts:
+    titleList.append(yt.get_attribute('title'))
+    hrefList.append(yt.get_attribute('href'))
+    time.sleep(1)
 
-# infos = miny_drive.find_elements(By.CSS_SELECTOR, "div#dismissible")
-# infos = miny_drive.find_elements(By.CSS_SELECTOR, value='a#video-title')
-# print ('-------------',infos)
-# for info in infos:
-#     title = info.text
-#     link = info.get_attribute('href')
-#     print(title, link)
+new_driver.close()
+
+df_yt = pd.DataFrame({
+    'yt_title' : titleList,
+    'yt_href' : hrefList
+})
+
+df_yt.head()
+print('>>>> end')
